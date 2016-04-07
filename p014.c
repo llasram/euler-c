9ee36a043d50582e20c945c898e1cb82790a5a8e
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2016-04-07 12:25:08 -0400

    Further improve performance of solution for #14

--- p014.c
+++ p014.c
@@ -12,25 +12,39 @@ solve(uint32_t n) {
   uint32_t bound = n - 2;
   uint32_t *num = malloc(bound * sizeof(uint32_t));
   uint32_t *state = malloc(bound * sizeof(uint32_t));
+  uint32_t *done = calloc(n, sizeof(uint32_t));
+
   for (uint32_t i = 0; i < bound; ++i) {
     state[i] = num[i] = i + 2;
   }
+  done[0] = done[1] = 1;
 
   uint32_t live = bound;
-  while (live > 1) {
+  uint32_t len_max = 0;
+  uint32_t len = 1;
+  while (live > 0) {
     uint32_t i_live = 0;
     for (uint32_t i = 0; i < live; ++i) {
+      uint32_t m = num[i];
       uint32_t x = state[i];
       x = (x % 2 == 0) ? (x / 2) : (3*x + 1);
-      if (x == 1) continue;
-      state[i_live] = x;
-      num[i_live] = num[i];
-      i_live += 1;
+      if (x < n && done[x] > 0) {
+        done[m] = len + done[x];
+        if (done[m] > len_max) {
+          len_max = done[m];
+          result = m;
+        }
+      } else {
+        state[i_live] = x;
+        num[i_live] = m;
+        i_live += 1;
+      }
     }
     live = i_live;
+    len += 1;
   }
-  result = num[0];
 
+  free(done);
   free(state);
   free(num);
 
